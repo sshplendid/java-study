97cb5288b678b73388959c4c27cb9b36b6eef255
SeHun Shin <s.shin@marathoner.kr>
2019-08-27 10:48:38 +0900

    동시성 예제

---
+++ java-example/src/test/java/example/AsyncTests.java
@@ -0,0 +1,67 @@
+package example;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class AsyncTests {
+    private Runnable echoAfter5Seconds = () -> {
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "....Hello!");
+    };
+
+    @Test
+    public void async1() throws ExecutionException, InterruptedException {
+        System.out.println("==test1");
+        CompletableFuture<Void> a =
+        CompletableFuture.runAsync(echoAfter5Seconds)
+                .thenCompose(aVoid -> CompletableFuture.runAsync(echoAfter5Seconds))
+                .thenAcceptAsync(aVoid -> System.out.println("Accept"))
+        .exceptionally(e -> {
+            System.out.println("Exception: ");
+            System.out.println(e.getMessage());
+            return null;
+        })
+        ;
+        a.whenComplete((aVoid, throwable) -> System.out.println("completed."));
+        a.get();
+        System.out.println("<=test1");
+    }
+
+    @Test
+    public void async2() throws ExecutionException, InterruptedException {
+        System.out.println("Start: " + LocalDateTime.now());
+        CompletableFuture<Void> task1 = CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("task 1 complete " + LocalDateTime.now());
+        });
+        CompletableFuture<Integer> task2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("task 2 complete. " + LocalDateTime.now());
+            return 1;
+        });
+
+        CompletableFuture.allOf(task1, task2).whenComplete((aVoid, throwable) -> {
+            System.out.println("All Completed " + LocalDateTime.now());
+        }).get();
+    }
+
+    @Test
+    public void async3() {
+    }
+}
---
+++ java-example/src/test/java/example/ThreadTests.java
@@ -0,0 +1,110 @@
+package example;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.concurrent.*;
+import java.util.logging.Logger;
+
+public class ThreadTests {
+    public static final Logger log = Logger.getAnonymousLogger();
+    private final PizzaService domino = new PizzaService();
+
+    @Test
+    public void pizzaTest() {
+        log.info("hungry");
+        domino.order("페페로니");
+        domino.deliver("성산동");
+        log.info("냠냠");
+    }
+
+    @Test
+    public void wrongTwoPizzasTest() {
+        log.info("hungry");
+        domino.order("슈프림");
+        domino.order("하와이안");
+        domino.deliver("올림픽로");
+    }
+
+    @Test
+    public void classicPizzaTest() throws InterruptedException {
+        Thread order1 = new Thread(() -> domino.order("슈프림"));
+
+        Thread order2 = new Thread(() -> domino.order("페페로니"));
+
+
+        order1.start();
+        order2.start();
+        order1.join();
+        order2.join();
+        domino.deliver("잠실");
+
+    }
+
+    @Test
+    public void modernTwoPizzaTest() throws InterruptedException {
+        FutureTask<Integer> order1 = new FutureTask(() -> domino.order("페페로니"));
+        FutureTask<Integer> order2 = new FutureTask(() -> domino.order("스테이크피자"));
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor.execute(order1);
+        executor.execute(order2);
+
+        while(!order1.isDone() || !order2.isDone()) {
+            Thread.sleep(1000);
+            log.info("tv를 보며 기다린다.");
+        }
+        domino.deliver("망원동");
+        log.info("냠냠");
+    }
+
+    @Test
+    public void modernTwoPizzaTest2() throws InterruptedException, ExecutionException, TimeoutException {
+        CompletableFuture<Void> pizza = CompletableFuture.allOf(
+                CompletableFuture.supplyAsync(() -> domino.order("페페로니")),
+                CompletableFuture.supplyAsync(() -> domino.order("슈프림")))
+                .thenRunAsync(() -> domino.deliver("신천동"))
+        .whenComplete((aVoid, throwable) -> log.info("냠냠"));
+        while(!pizza.isDone()) {
+            log.info("Waiting...");
+            Thread.sleep(1000);
+        }
+    }
+    @Test
+    public void test() {
+        LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli();
+    }
+
+
+}
+
+class PizzaService {
+    public static final Logger log = Logger.getAnonymousLogger();
+
+    public int order(String pizzaType) {
+        log.info("피자 주문 완료: " + pizzaType);
+        int waitingTime = pizzaType.length();
+        log.info("피자 만드는 중..." + pizzaType);
+        try {
+            TimeUnit.SECONDS.sleep(waitingTime);
+            log.info("피자 완료: " + pizzaType);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return waitingTime;
+    }
+
+    public int deliver(String address) {
+        log.info("배달중...: " + address);
+        int waitingTime = address.length();
+        try {
+            TimeUnit.SECONDS.sleep(waitingTime);
+            log.info("피자 배달 완료");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return waitingTime;
+    }
+
+}
